8a12dacee9dc72d1baa32e456a8d4558ccca8e88
Michael Romay <amerror@gmail.com>
2020-02-04 16:09:59 -0800

    testing visuals

--- synth/synth.js
+++ synth/synth.js
@@ -16,6 +16,31 @@ let releasePlayback = 0.05;   // release speed
 let portamentoPlayback = 0;  // portamento/glide speed
 let recordStartTime = null;
 
+/////
+
+var mouseX = 0,
+	mouseY = 0,
+	windowHalfX = window.innerWidth / 2,
+	windowHalfY = window.innerHeight / 2,
+	camera,
+	scene,
+	renderer,
+	material,
+	container;
+var source;
+
+var buffer;
+var audioBuffer;
+var dropArea;
+var audioContext;
+var source;
+//var processor;
+// var analyser;
+var xhr;
+var started = true;
+
+
+
 
 // DOM RECORD BUTTONS
 const recordStartButton = document.getElementById('recordButton');
@@ -40,6 +65,10 @@ document.addEventListener('DOMContentLoaded', function(event) {
 
     //SETUP OSCILLOSCOPE
     const myOscilloscope = new OScope(audioCtx, 'oscilloscope');
+
+    let analyser = audioCtx.createAnalyser();
+	analyser.fftSize = 1024;
+	analyser.smoothingTimeConstant = 0.1;
   
     //PROCESSING CHAIN
     const gain = audioCtx.createGain();
@@ -69,8 +98,8 @@ document.addEventListener('DOMContentLoaded', function(event) {
         '76': 587.329535834815120,  //L - D
         '80': 622.253967444161821, //P - D#
         '186': 659.255113825739859,  //; - E
-        '219': 698.456462866007768,  //[ - F
-        '222': 739.988845423268797, //' - F#
+        '222': 698.456462866007768,  //' - F
+        '221': 739.988845423268797, //] - F#
         // '84': 783.990871963498588,  //T - G
         // '54': 830.609395159890277, //6 - G#
         // '89': 880.000000000000000,  //Y - A
@@ -81,10 +110,16 @@ document.addEventListener('DOMContentLoaded', function(event) {
 
   
     //CONNECTIONS
-    gain.connect(filterLP);
-    filterLP.connect(filterHP);
-    filterHP.connect(filterBP);
-    filterBP.connect(myOscilloscope);
+    gain.connect(analyser);
+    // gain.connect(filterLP);
+    // filterLP.connect(filterHP);
+    // filterHP.connect(filterBP);
+    // filterBP.connect(myOscilloscope);
+
+    // source.connect(analyser);
+    // analyser.connect(myOscilloscope);
+    analyser.connect(audioCtx.destination);
+    
     myOscilloscope.connect(audioCtx.destination);
   
     //EVENT LISTENERS FOR SYNTH PARAMETER INTERFACE
@@ -92,28 +127,28 @@ document.addEventListener('DOMContentLoaded', function(event) {
         waveform = event.target.value;
     });
   
-    gainControl.addEventListener('change', function(event) {
+    gainControl.addEventListener('mousemove', function(event) {
         gain.gain.setValueAtTime(event.target.value, audioCtx.currentTime);
     });
 
-    frequencyControlLP.addEventListener('change', function(event) {
+    frequencyControlLP.addEventListener('mousemove', function(event) {
         filterLP.type = 'lowpass';
         filterLP.frequency.setValueAtTime(event.target.value, audioCtx.currentTime);
     });
 
-    frequencyControlHP.addEventListener('change', function(event) {
+    frequencyControlHP.addEventListener('mousemove', function(event) {
         filterHP.type = 'highpass';
         filterHP.frequency.setValueAtTime(event.target.value, audioCtx.currentTime);
     });
 
-    frequencyControlBP.addEventListener('change', function(event) {
+    frequencyControlBP.addEventListener('mousemove', function(event) {
         filterBP.type = 'bandpass';
         filterBP.frequency.setValueAtTime(event.target.value, audioCtx.currentTime);
     });
 
-    lfoControl.addEventListener('change', function(event) {
-        lfo.frequency.setValueAtTime(event.target.value, audioCtx.currentTime);
-    });
+    // lfoControl.addEventListener('mousemove', function(event) {
+    //     lfo.frequency.setValueAtTime(event.target.value, audioCtx.currentTime);
+    // });
   
     //EVENT LISTENERS FOR MUSICAL KEYBOARD
     window.addEventListener('keydown', keyDown, false);
@@ -121,8 +156,10 @@ document.addEventListener('DOMContentLoaded', function(event) {
   
     //CALLED ON KEYDOWN EVENT - CALLS PLAYNOTE IF KEY PRESSED IS ON MUSICAL
     //KEYBOARD && THAT KEY IS NOT CURRENTLY ACTIVE
-    function keyDown(event) {
+    function keyDown(event, e) {
         const key = (event.detail || event.which).toString();
+        const keyImage = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+        keyImage.classList.add('active');
 
         keyObject = {
             note_switch: 144,
@@ -139,8 +176,10 @@ document.addEventListener('DOMContentLoaded', function(event) {
   
     //STOPS & DELETES OSCILLATOR ON KEY RELEASE IF KEY RELEASED IS ON MUSICAL
     //KEYBOARD && THAT KEY IS CURRENTLY ACTIVE
-    function keyUp(event) {
+    function keyUp(event, e) {
         const key = (event.detail || event.which).toString();
+        const keyImage = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+        keyImage.classList.remove('active');
 
         keyObject = {
             note_switch: 128,
@@ -251,10 +290,10 @@ document.addEventListener('DOMContentLoaded', function(event) {
 
     //RECORDING
 
-    recordStartButton.addEventListener('click', () => {
-        musicalLayer = [];
-        recordStartTime = audioCtx.currentTime;
-    });
+    // recordStartButton.addEventListener('click', () => {
+    //     musicalLayer = [];
+    //     recordStartTime = audioCtx.currentTime;
+    // });
     
     function storingMusic(musicObject) {
         musicalLayer.push(musicObject);
@@ -319,3 +358,230 @@ document.addEventListener('DOMContentLoaded', function(event) {
         }
     }
 });
+
+///////////////////
+/**
+ *
+ * Loop Waveform Visualizer by Felix Turner
+ * www.airtight.cc
+ *
+ * Audio Reactive Waveform via Web Audio API.
+ *
+ */
+
+
+
+$(document).ready(function() {
+	//Chrome is only browser to currently support Web Audio API
+	var is_webgl = (function() {
+		try {
+			return !!window.WebGLRenderingContext && !!document.createElement('canvas').getContext('experimental-webgl');
+		} catch (e) {
+			return false;
+		}
+	})();
+
+	if (!is_webgl) {
+		$('#loading').html(
+			'Your graphics card does not seem to support <a href="http://khronos.org/webgl/wiki/Getting_a_WebGL_Implementation">WebGL</a>.<br />' +
+				'Find out how to get it <a href="http://get.webgl.org/">here</a>, or try restarting your browser.'
+		);
+	} else {
+		$('#loading').html('drop mp3 here or <a id="loadSample">load sample mp3</a>');
+		init();
+	}
+});
+
+function init() {
+	//init 3D scene
+	container = document.createElement('div');
+	document.body.appendChild(container);
+	camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 1, 1000000);
+	camera.position.z = 350;
+	scene = new THREE.Scene();
+	scene.add(camera);
+	renderer = new THREE.WebGLRenderer({
+		antialias: false,
+		sortObjects: false,
+	});
+	renderer.setSize(window.innerWidth, window.innerHeight);
+
+	container.appendChild(renderer.domElement);
+
+	// stop the user getting a text cursor
+	document.onselectStart = function() {
+		return false;
+	};
+
+	//add stats
+	// stats = new Stats();
+	// stats.domElement.style.position = 'absolute';
+	// stats.domElement.style.top = '0px';
+	// container.appendChild(stats.domElement);
+
+	//init listeners
+	$('#loadSample').click(loadSampleAudio);
+	$(document).mousemove(onDocumentMouseMove);
+	$(window).resize(onWindowResize);
+	document.addEventListener('drop', onDocumentDrop, false);
+	document.addEventListener('dragover', onDocumentDragOver, false);
+
+	onWindowResize(null);
+}
+
+function loadSampleAudio() {
+	$('#loading').text('loading...');
+
+	audioContext = new window.AudioContext();
+
+	source = audioContext.createBufferSource();
+	analyser = audioContext.createAnalyser();
+	analyser.fftSize = 1024;
+	analyser.smoothingTimeConstant = 0.1;
+
+	// Connect audio processing graph
+	// source.connect(analyser);
+	// analyser.connect(audioContext.destination);
+
+	loadAudioBuffer('audio/EMDCR.mp3');
+}
+
+function loadAudioBuffer(url) {
+	// Load asynchronously
+	var request = new XMLHttpRequest();
+	request.open('GET', url, true);
+	request.responseType = 'arraybuffer';
+
+	request.onload = function() {
+		audioContext.decodeAudioData(
+			request.response,
+			function(buffer) {
+				audioBuffer = buffer;
+				finishLoad();
+			},
+			function(e) {
+				console.log(e);
+			}
+		);
+	};
+	request.send();
+}
+
+function finishLoad() {
+	source.buffer = audioBuffer;
+	source.loop = true;
+	source.start(0.0);
+	startViz();
+}
+
+function onDocumentMouseMove(event) {
+	mouseX = (event.clientX - windowHalfX) * 2;
+	mouseY = (event.clientY - windowHalfY) * 2;
+}
+
+function onWindowResize(event) {
+	windowHalfX = window.innerWidth / 2;
+	windowHalfY = window.innerHeight / 2;
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
+	renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+function animate() {
+	requestAnimationFrame(animate);
+	render();
+	//stats.update();
+}
+
+function render() {
+	LoopVisualizer.update();
+
+	//mouse tilt
+	var xrot = mouseX / window.innerWidth * Math.PI + Math.PI;
+	var yrot = mouseY / window.innerHeight * Math.PI + Math.PI;
+	LoopVisualizer.loopHolder.rotation.x += (-yrot - LoopVisualizer.loopHolder.rotation.x) * 0.3;
+	LoopVisualizer.loopHolder.rotation.y += (xrot - LoopVisualizer.loopHolder.rotation.y) * 0.3;
+
+	renderer.render(scene, camera);
+}
+
+$(window).mousewheel(function(event, delta) {
+	//set camera Z
+	camera.position.z -= delta * 50;
+});
+
+function onDocumentDragOver(evt) {
+	evt.stopPropagation();
+	evt.preventDefault();
+	return false;
+}
+
+function onDocumentDrop(evt) {
+	evt.stopPropagation();
+	evt.preventDefault();
+
+	//clean up previous mp3
+	if (source) source.disconnect();
+	LoopVisualizer.remove();
+
+	$('#loading').show();
+	$('#loading').text('loading...');
+
+	var droppedFiles = evt.dataTransfer.files;
+
+	var reader = new FileReader();
+
+	reader.onload = function(fileEvent) {
+		var data = fileEvent.target.result;
+		initAudio(data);
+	};
+
+	reader.readAsArrayBuffer(droppedFiles[0]);
+}
+
+function initAudio(data) {
+	audioContext = new window.AudioContext();
+	source = audioContext.createBufferSource();
+
+	if (audioContext.decodeAudioData) {
+		audioContext.decodeAudioData(
+			data,
+			function(buffer) {
+				source.buffer = buffer;
+				createAudio();
+			},
+			function(e) {
+				console.log(e);
+				$('#loading').text('cannot decode mp3');
+			}
+		);
+	} else {
+		source.buffer = audioContext.createBuffer(data, false);
+		createAudio();
+	}
+}
+
+function createAudio() {
+	let analyser = audioContext.createAnalyser();
+	analyser.fftSize = 1024;
+	analyser.smoothingTimeConstant = 0.1;
+	source.connect(audioContext.destination);
+	source.connect(analyser);
+	source.start(0);
+	source.loop = true;
+
+	startViz();
+}
+
+function startViz() {
+	$('#loading').hide();
+
+	LoopVisualizer.init();
+
+	if (!started) {
+		started = true;
+		animate();
+	}
+}
+startViz();
+/////////////
